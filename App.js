d164239f7ff889fafc828568e25959ac3773d4d1
Anoop Pandey <apandey@getquantic.com>
2023-08-23 03:51:59 +0530

    Episode-2 done

--- App.js
+++ App.js
@@ -1,18 +1,5 @@
-/*
-    <div id="parent">
-        <div id="child">
-            <h1>i am a h1 tag</h1>
-            <h2>this is h2 tag</h2>
-        </div>
-        <div id="child2">
-            <h1>i am a h1 tag</h1>
-            <h2>this is h2 tag</h2>
-        </div>
-    </div>
-
-    ReactElement(Object) => HTML(Browser Understands)
-*/
-
+import React from "react";
+import ReactDOM  from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" },
     [React.createElement("div", { id: "child" },
@@ -22,8 +9,6 @@ const parent = React.createElement("div", { id: "parent" },
         [React.createElement("h1", {}, "i am a h1 tag"), React.createElement("h2", {}, "i am a h1 tag")]
     )]
 );
-
-// After that messing code above need jsx come in to the picture and say bye-2 to React Create Element
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Namste React
+# Namste React 
+
+# Parcel
+- It's Create Dev Build
+- It's Create Local Server
+- It's Create HMR = Hot Module Replacement
+- File Watching Algorithms = Written in C++
+- Caching = Faster Builds
+- Image Optimization
+- Minification Files and Bundling
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling to Support Older Browsers
